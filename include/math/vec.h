ec7a927b706a505feabc28faf074131ec1d6a6fe
TrenkaOliver <156715125+TrenkaOliver@users.noreply.github.com>
2026-06-23 14:13:38 +0200

    inlining vec functions

--- include/math/vec.h
+++ include/math/vec.h
@@ -1,33 +1,64 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <math.h>
+
 typedef struct Vec {
     double x;
     double y;
     double z;
 } Vec;
 
-Vec vec(double x, double y, double z);
+static inline Vec vec(double x, double y, double z) {
+    return (Vec) {.x = x, .y = y, .z = z};
+}
 
-double len_sqr(Vec vec);
+static inline double len_sqr(Vec vec) {
+    return vec.x * vec.x + vec.y * vec.y + vec.z * vec.z;
+}
+
+static inline Vec v_min(Vec a, Vec b) {
+    return (Vec){.x = fmin(a.x, b.x), .y = fmin(a.y, b.y), .z = fmin(a.z, b.z)};
+}
+
+static inline Vec v_max(Vec a, Vec b) {
+    return (Vec){.x = fmax(a.x, b.x), .y = fmax(a.y, b.y), .z = fmax(a.z, b.z)};
+}
+
+static inline Vec neg(Vec vec) {
+    return (Vec){.x = -vec.x, .y = -vec.y, .z = -vec.z};
+}
+
+static inline Vec v_add(Vec a, Vec b) {
+    return (Vec){.x = a.x + b.x, .y = a.y + b.y, .z = a.z + b.z};
+}
+
+static inline Vec v_sub(Vec a, Vec b) {
+    return (Vec){.x = a.x - b.x, .y = a.y - b.y, .z = a.z - b.z,};
+}
+
+static inline Vec scale(Vec vec, double value) {
+    return (Vec){.x = vec.x * value, .y = vec.y * value, .z = vec.z * value,};
+}
+
+static inline double dot(Vec a, Vec b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+static inline Vec hadamard(Vec a, Vec b) {
+    return (Vec){.x = a.x * b.x, .y = a.y * b.y, .z = a.z * b.z};
+}
+
+static inline Vec cross(Vec a, Vec b) {
+    return (Vec){
+        .x = a.y * b.z - a.z * b.y,
+        .y = a.z * b.x - a.x * b.z,
+        .z = a.x * b.y - a.y * b.x
+    };
+}
+
 double len(Vec vec);
-
-Vec v_min(Vec a, Vec b);
-Vec v_max(Vec a, Vec b);
-
-Vec neg(Vec vec);
 Vec normalize(Vec vec);
-
-Vec v_add(Vec a, Vec b);
-Vec v_sub(Vec a, Vec b);
-
-Vec scale(Vec vec, double value);
-Vec v_div(Vec vec, double value);
-
-double dot(Vec a, Vec b);
-Vec cross(Vec a, Vec b);
-Vec hadamard(Vec a, Vec b);
-
 Vec rotate(Vec v, Vec r);
 
 #endif
--- src/math/vec.c
+++ src/math/vec.c
@@ -1,101 +1,17 @@
 #include "math.h"
 #include "math/vec.h"
 
-Vec vec(double x, double y, double z) {
-    return (Vec) {
-        .x = x, 
-        .y = y, 
-        .z = z
-    };
-}
-
-double len_sqr(Vec vec) {
-    return vec.x * vec.x + vec.y * vec.y + vec.z * vec.z;
-}
-
 double len(Vec vec) {
     return sqrt(len_sqr(vec));
-}
-
-Vec v_min(Vec a, Vec b) {
-    return (Vec){
-        .x = fmin(a.x, b.x),
-        .y = fmin(a.y, b.y),
-        .z = fmin(a.z, b.z),
-    };
-}
-
-Vec v_max(Vec a, Vec b) {
-    return (Vec){
-        .x = fmax(a.x, b.x),
-        .y = fmax(a.y, b.y),
-        .z = fmax(a.z, b.z),
-    };
-}
-
-Vec neg(Vec vec) {
-    return scale(vec, -1);
 }
 
 Vec normalize(Vec vec) {
     double l;
 
-    if ((l = len(vec)) == 0) 
+    if ((l = len(vec)) == 0 || l == 1) 
         return vec;
     else 
-        return v_div(vec, l);
-}
-
-Vec v_add(Vec a, Vec b) {
-    return (Vec) {
-        .x = a.x + b.x, 
-        .y = a.y + b.y,
-        .z = a.z + b.z,
-    };
-}
-
-Vec v_sub(Vec a, Vec b) {
-    return (Vec) {
-        .x = a.x - b.x, 
-        .y = a.y - b.y,
-        .z = a.z - b.z,
-    };
-}
-
-Vec scale(Vec vec, double value) {
-    return (Vec) {
-        .x = vec.x * value,
-        .y = vec.y * value,
-        .z = vec.z * value,
-    };
-}
-
-Vec v_div(Vec vec, double value) {
-    return (Vec) {
-        .x = vec.x / value,
-        .y = vec.y / value,
-        .z = vec.z / value,
-    };
-}
-
-double dot(Vec a, Vec b) {
-    return a.x * b.x + a.y * b.y + a.z * b.z;
-}
-
-Vec cross(Vec a, Vec b) {
-    return (Vec) {
-        .x = a.y * b.z - a.z * b.y,
-        .y = a.z * b.x - a.x * b.z,
-        .z = a.x * b.y - a.y * b.x
-    };
-}
-
-Vec hadamard(Vec a, Vec b) {
-    return (Vec) {
-        .x = a.x * b.x,
-        .y = a.y * b.y,
-        .z = a.z * b.z,
-    };
+        return scale(vec, 1.0 / l);
 }
 
 Vec rotate(Vec v, Vec r) {
